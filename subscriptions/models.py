927816bc11e9354b10dd13914848972aeaba95d6
moonpie46 <jluna@orangecompute.com>
2023-05-17 20:58:01 -0700

    added subscriptions

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+User=get_user_model()
 # Create your models here.
+
+class subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    price= models.IntegerField()
+
+    def __str__(self):
+        return self.name
