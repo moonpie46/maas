85bfc7a0695233f63dfbe8c8285ef80c306f813f
moonpie46 <jluna@orangecompute.com>
2023-05-18 17:30:20 -0700

    models

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -6,6 +6,8 @@ User=get_user_model()
 class subscription(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
     price= models.IntegerField()
 
     def __str__(self):
