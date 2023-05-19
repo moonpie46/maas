23f557d99bbd57598005402afdab287be8a494ec
moonpie46 <jluna@orangecompute.com>
2023-05-19 09:56:13 -0700

    added new login forms

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -17,3 +17,10 @@ def profile(request):
 
 def home(request):
     return render(request, 'home.html')
+
+def login(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/accounts/profile')
